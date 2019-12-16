60149e97c9f0a1c8ea2b7c1a6ab46a8388155e94
dimin0514 <dimin0514@gmail.com>
2019-12-16 18:13:04 +0900

    first commit

---
+++ src/main/java/com/catdog/web/CatdogSbvApplication.java
@@ -0,0 +1,13 @@
+package com.catdog.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatdogSbvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatdogSbvApplication.class, args);
+	}
+
+}
